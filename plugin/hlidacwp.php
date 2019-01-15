a8e4fbb1f0db263d3230ecdd18cb7f2b31ca11cd
Michal Blaha <michal@michalblaha.cz>
2019-01-15 13:10:34 +0100

    Update hlidacwp.php

--- plugin/hlidacwp.php
+++ plugin/hlidacwp.php
@@ -3,11 +3,11 @@
 Plugin Name: Hlídač Státu
 Plugin URI:
 Description:
-Version: 0.1
-Author: Hlídač Státu
-Author URI:
+Version: 0.2
+Author: Hlídač Státu, Petr Štastný
+Author URI:https://github.com/HlidacStatu/Wordpress-plugin/
 */
 
 require_once __DIR__.'/HlidacWidget.php';
 
-add_shortcode('hlidac-widget', array('\\Hlidac\\Wp\\HlidacWidget', 'getContent'));
+add_shortcode('hlidac-statu', array('\\Hlidac\\Wp\\HlidacWidget', 'getContent'));
